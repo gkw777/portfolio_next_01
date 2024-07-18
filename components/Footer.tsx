3ec949145fef86a6af0a4bdde16c338dc6a1d8b8
KyungWonK <gkw777@gmail.com>
2024-07-18 12:45:22 +0900

    아직 구현되지 않은 footer 내용 주석처리

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,7 @@
-import { footerLinks } from '@/constants';
-import Image from 'next/image';
-import Link from 'next/link';
-
 const Footer = () => {
   return (
     <footer className='flex flex-col text-black-100 border-t border-gray-100 mt-5'>
-      <div className='flex max-md:flex-col flex-wrap justify-between gap-5 padding-x py-10'>
+      {/* <div className='flex max-md:flex-col flex-wrap justify-between gap-5 padding-x py-10'>
         <div className='flex flex-col justify-start items-start gap-6'>
           <Image src='/logo.png' alt='logo' width={250} height={115} priority className='object-contain' />
           <p className='text-base text-gray-700'>
@@ -27,17 +23,17 @@ const Footer = () => {
             </div>
           ))}
         </div>
-      </div>
-      <div className='flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 padding-x py-10 gap-2'>
+      </div> */}
+      <div className='padding-x py-10'>
         <p>@2024 github.com/gkw777 All rights reserved</p>
-        <div className='footer__copyrights-link'>
+        {/* <div className='footer__copyrights-link'>
           <Link href='/' className='text-gray-500 text-nowrap'>
             Privacy & Policy
           </Link>
           <Link href='/' className='text-gray-500  text-nowrap'>
             Terms & Condition
           </Link>
-        </div>
+        </div> */}
       </div>
     </footer>
   );
